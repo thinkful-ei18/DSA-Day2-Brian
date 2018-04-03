1d2e50583f958b041deef718c68a71e89e0f00d4
BrianDudey <dudey.brian@gmail.com>
2018-04-03 17:07:33 -0500

    commit 2 of 2

--- BigODrills.js
+++ BigODrills.js
@@ -50,5 +50,65 @@
 
 //Is it Prime?
 //This is O(2) - Linear Time
-//The reason this is linear time is because it has the potential to take as long 
+//The reason this is linear time is because it has the potential to take as long as 
+//the input since the input may only be divisible by itself. Therefore, the function
+//will iterate 1 to n times.
+
+/////Yesterday's Exercise
+
+//Counting Sheep
+//Recursive - this is O(n) linear time because the number of operations is dependant
+//on the input - larger input is an equal number of operations.
+//Iterable - this is also O(n) linear time for the same reasons - if 3 is the input there
+//will be three operations but if six is the input, there will be six operations.
+//it is 1:1
+
+//Array Double
+//Recursive - this is O(n) because it is being asked to double all of the numbers in the
+//array. Therefore, if the number of operations is equal to the number of variables
+//in the array.
+//Iterable - This is also O(n) because it is going through the array one time but
+//has to perform the doubling operation for each item in the array - 17 items in the array
+//equates to 17 times the function must double the number. It is 1:1
+
+//Backwards String
+//Recursive - This is O(n^2) polynomial time because it takes (approximately) twice as long
+//as the number of characters in the string so 'cat' is approximately six operations
+//wheras 'tigers' is approximately twelve operations (taking two tines as long).
+//Iterable - This is also O(n^2) because it requires two operations per letter.
+
+//Count the triangle
+//Recursive - This is polynomial time O(n^2) because it has approxinmately twice as many operations
+//as the input size. n=3 is approximately half the number of operations as n=6
+//Iterable - This is also O(2^n) because it takes a significant amount more time to process a larger number
+
+//String Splitter
+//Recursive - this is O(n) because the number of operations can be, in the worst case, as long as the number
+//used as the input.
+//Iterable - This is O(n^2) because it takes at least twice as long as the input to complete
+//the function.
+
+//Binary Representation
+//Recursive - this is O(log(n)) because it does not change in linear time based on the input
+//size. Instead, it grows a fraction of the size of the inout.
+//Iterable - This is also O(log(n)) because the number of operations do grow but they grow much
+//at a fraction of the input size.
+
+//Factorial
+//Recursive - this is O(n^2) because it the number of operations increaes approximately ^2 times
+//the number of the input - 78 takes 240 operations whilte 39 takes 120
+//Iterable - this is also O(n^2) for the exact reasons the recurive version is.
+
+//Fibonacci
+//Recursive - this is exponential time O(2^n) because it takes a significantly longer time
+//for the function to complete even when the input is only slightly increased.
+//Iterable - this is O(n) because unline the recursive version it steadily increases 
+//based on an increase in the input. The increase is significant enough to not be O(log(n))
+
+//Anagrams
+//This is 
+
+//Animal Hierarchy
+//This is O(n) because the number of operations grows in a linear fashion based on the size
+//of the input.
 
